f8a9f1060df31babc2669cdb3110983369c2ae98
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2022-11-10 21:01:18 -0500

    does this work

--- sanity.ts
+++ sanity.ts
@@ -2,9 +2,8 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 const config = {
-    projectId: "wfo9nqzf",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, 
+    projectId: process.env.wfo9nqzf, 
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
 }
