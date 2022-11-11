0ca53427f646b53f6f9b33f29f5b561a5c7d1503
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2022-11-10 20:55:01 -0500

    please work

--- sanity.ts
+++ sanity.ts
@@ -2,6 +2,7 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 const config = {
+    projectId: "wfo9nqzf",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, 
     apiVersion: "2021-03-25",
