30ba09a32312f89a24d3d46e2bc14f599d25d1ad
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2022-11-10 21:51:50 -0500

    shot in the fucking dark

--- sanity.ts
+++ sanity.ts
@@ -1,6 +1,7 @@
 import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
+
 export const config = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, 
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
@@ -8,7 +9,7 @@ export const config = {
     useCdn: process.env.NODE_ENV === "production",
 }
 
-export const sanityClient = createClient(config);
+export const sanityClient = createClient( config);
 
 export const urlFor = (source: any ) => 
     createImageUrlBuilder(config).image(source);
